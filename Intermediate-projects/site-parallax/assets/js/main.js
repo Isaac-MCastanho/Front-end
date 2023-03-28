7db4b7d786e93c82d54c5067ab722b1c09b60c2f
Isaac-MCastanho <fgcastanho@hotmail.com>
2023-03-27 22:21:28 -0300

    site-parallax - efeito parallax OK

--- Intermediate-projects/site-parallax/assets/js/main.js
+++ Intermediate-projects/site-parallax/assets/js/main.js
@@ -1,1 +1,17 @@
-
+let stars = document.querySelector("#stars");
+let moon = document.querySelector("#moon");
+let mountains_behind = document.querySelector("#mountains_behind");
+let text = document.querySelector("#text");
+let btn = document.querySelector("#btn");
+let mountains_front = document.querySelector("#mountains_front");
+let header = document.querySelector("header");
+document.addEventListener("scroll", () => {
+  let value = window.scrollY;
+  stars.style.left = value * 0.25 + "px";
+  moon.style.top = value * 1.05 + 100 + "px";
+  mountains_behind.style.top = value * 0 + "px";
+  mountains_front.style.top = value * 0.05 + "px";
+  text.style.marginRight = value * 4 + "px";
+  text.style.marginTop = value * 1.5 + "px";
+  btn.style.marginTop = value * 1.5 + "px";
+});
