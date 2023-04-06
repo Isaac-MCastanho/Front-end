f9154424268be455933179e914d65283d174a023
Isaac-MCastanho <fgcastanho@hotmail.com>
2023-04-06 17:07:51 -0300

    form

--- basic-projects/Gen-site/assets/js/main.js
+++ basic-projects/Gen-site/assets/js/main.js
@@ -1,4 +1,7 @@
 const miniRodape = document.querySelector(".miniRodape");
+const nome = document.querySelector("#nome");
+const email = document.querySelector("#email");
+const assunto = document.querySelector("#assunto");
 
 document.addEventListener("scroll", () => {
   if (window.scrollY > 100) {
@@ -7,3 +10,15 @@ document.addEventListener("scroll", () => {
     miniRodape.classList.remove("miniRodape--active");
   }
 });
+
+function validaNome() {
+  if (nome.value.length < 3) {
+    alert("nome invalido");
+  }
+}
+
+function validaEmail() {
+  if (email.value.indexOf("@") == -1) {
+    alert("nome invalido");
+  }
+}
